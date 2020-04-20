8d79180396a6e63c50ae9e5f4b5658e7bdb03be5
Magesh Sarma - AmeriSave <msarma@winprosoftware.com>
2020-04-20 12:59:26 -0400

    Add Text Spinner

---
+++ textspinner/app/endpoint.py
@@ -0,0 +1,27 @@
+from flask import jsonify
+from app import app
+import re
+import sys
+import itertools
+import more_itertools
+
+def options(s):
+	if len(s) > 0 and s[0] == '{':
+		return [opt for opt in s[1:-1].split('|')]
+
+	return [s]
+
+@app.route("/spin/<message>", defaults={'message': ''}, methods=['GET'])
+def index(message):
+  try:
+    chunk = re.split('(\{[^\}]+\}|[^\{\}]+)', message)
+
+    opt_lists = [options(frag) for frag in chunk]
+
+    text = ''
+    for spec in more_itertools.random_product(*opt_lists):
+      text = text + (''.join(spec))
+
+    return text
+  except Exception as e:
+    return jsonify(str(e))
