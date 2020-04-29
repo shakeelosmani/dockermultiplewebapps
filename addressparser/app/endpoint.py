dfc860fc23d18c7031aaa71d9bc940f20dab2b77
msarma <msarma@amerisave.com>
2020-04-29 14:55:01 -0400

    New end points

--- addressparser/app/endpoint.py
+++ addressparser/app/endpoint.py
@@ -5,11 +5,49 @@ import sys
 import usaddress
 
 @app.route("/parseaddress", methods=['POST'])
-def index():
+def parseaddress():
   try:
     inp = request.json
     address = inp["address"]
     processedaddress = usaddress.tag(address)
+
+    return jsonify(processedaddress)
+  except Exception as e:
+    return jsonify(str(e))
+
+@app.route("/parseaddresswithmapping", methods=['POST'])
+def parseaddresswithmapping():
+  try:
+    inp = request.json
+    address = inp["address"]
+    processedaddress = usaddress.tag(address, tag_mapping = {
+      'Recipient': 'recipient',
+      'AddressNumber': 'address1',
+      'AddressNumberPrefix': 'address1',
+      'AddressNumberSuffix': 'address1',
+      'StreetName': 'address1',
+      'StreetNamePreDirectional': 'address1',
+      'StreetNamePreModifier': 'address1',
+      'StreetNamePreType': 'address1',
+      'StreetNamePostDirectional': 'address1',
+      'StreetNamePostModifier': 'address1',
+      'StreetNamePostType': 'address1',
+      'CornerOf': 'address1',
+      'IntersectionSeparator': 'address1',
+      'LandmarkName': 'address1',
+      'USPSBoxGroupID': 'address1',
+      'USPSBoxGroupType': 'address1',
+      'USPSBoxID': 'address1',
+      'USPSBoxType': 'address1',
+      'BuildingName': 'address2',
+      'OccupancyType': 'address2',
+      'OccupancyIdentifier': 'address2',
+      'SubaddressIdentifier': 'address2',
+      'SubaddressType': 'address2',
+      'PlaceName': 'city',
+      'StateName': 'state',
+      'ZipCode': 'zipcode'
+    })
 
     return jsonify(processedaddress)
   except Exception as e:
